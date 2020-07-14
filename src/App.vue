2f37b9eda40f85b65e8d56470209f2eac45701ab
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-07-14 18:59:07 -0400

    ADD: VueMaterial, realizados ambos bonus del ejercicio

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app" class="background">
     <div class="container">
-      <h2>
-        Encuesta del Dojo
-      </h2>
+      <h2>ENCUESTA DEL DOJO</h2>
       <Formularios />
     </div>
   </div>
@@ -18,14 +16,27 @@ export default {
     Formularios
   },
   data: () => {
-    return {
-      
-    };
+    return {};
   }
 };
 </script>
 
-<style>
+<style lang='scss'>
+
+@import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+@include md-register-theme("default", (
+  primary: #80ffc3, // The primary color of your application
+  accent: #964ee9, // The accent or secondary color
+  theme: dark // This can be dark or light
+));
+
+@import "~vue-material/dist/components/MdList/theme"; // Apply the Button theme
+@import "~vue-material/dist/components/MdButton/theme"; // Apply the Button theme
+@import "~vue-material/dist/components/MdField/theme"; // Apply the Button theme
+@import "~vue-material/dist/components/MdRadio/theme"; // Apply the Button theme
+@import "~vue-material/dist/components/MdDialog/theme"; // Apply the Button theme
+
 body {
   margin: 0 !important;
 }
@@ -40,25 +51,31 @@ body {
   position: absolute;
   height: 100%;
   width: 100%;
-  background: linear-gradient(to top, #360033, #0b8793);
+  overflow: auto;
+  background: linear-gradient(to top, #240422, #007e8a) fixed;
 }
 .container {
-  background-color: rgba(53, 53, 53, 0.102);
-  border-radius: 10px;
+  background-color: rgba(12, 12, 12, 0.15);
+  border-radius: 5px;
   padding: 25px;
-  margin-top: 3rem;
+  padding-bottom: 5px;
+  margin: 0.5rem auto;
+  overflow: hidden;
   backdrop-filter: blur(5px);
-  box-shadow: -7px -7px 23px -4px rgba(0, 0, 0, 0.2);
-  max-width: 280px;
+  max-width: 310px;
   position: absolute;
   left: 0;
   right: 0;
-  margin-left: auto;
-  margin-right: auto;
-  color: #e8f9ff;
+  color: #ffffff;
 }
 h2 {
-  margin-bottom: 2rem;
   text-align: center;
+}
+.md-list.md-theme-default {
+  background-color: #381a37f3;
+}
+.md-dialog-container.md-theme-default {
+  background-color: #381a37f3;
+  color: #fff;
 }
 </style>
--- src/components/Formularios.vue
+++ src/components/Formularios.vue
@@ -1,31 +1,57 @@
 <template>
   <div>
-    <p>Tu Nombre:</p>
-    <input type="text" v-model="nombre"/>
+    <md-field>
+      <label>Nombre</label>
+      <md-input type="text" v-model="nombre" />
+    </md-field>
 
-    <p>Ubicación:</p>
-    <select v-model="ubicacion">
-      <option value="antofagasta">Antofagasta</option>
-      <option value="valparaiso">Valparaíso</option>
-      <option value="santiago">Santiago</option>
-      <option value="concepcion">Concepción</option>
-      <option value="valdivia">Valdivia</option>
-    </select>
+    <md-field>
+      <label>Edad</label>
+      <md-input type="number" v-model="edad" />
+    </md-field>
 
-    <p>Lenguaje Favorito:</p>
-    <select v-model="lenguaje">
-      <option value="javascript">Javascript</option>
-      <option value="java">Java</option>
-      <option value="python">Python</option>
-      <option value="c#">C#</option>
-      <option value="swift">Swift</option>
-      <option value="ruby">Ruby</option>
-    </select>
+    <md-field>
+      <label>Ubicación</label>
+      <md-select v-model="ubicacion">
+        <md-option value="Antofagasta">Antofagasta</md-option>
+        <md-option value="Valparaíso">Valparaíso</md-option>
+        <md-option value="Santiago">Santiago</md-option>
+        <md-option value="Concepción">Concepción</md-option>
+        <md-option value="Valdivia">Valdivia</md-option>
+      </md-select>
+    </md-field>
 
-    <p>Comentarios:</p>
-    <textarea v-model="comentarios"></textarea>
+    <md-field>
+      <label>Lenguaje Favorito</label>
+      <md-select v-model="lenguaje">
+        <md-option value="JavaScript">JavaScript</md-option>
+        <md-option value="Java">Java</md-option>
+        <md-option value="Python">Python</md-option>
+        <md-option value="C#">C#</md-option>
+        <md-option value="Swift">Swift</md-option>
+        <md-option value="Ruby">Ruby</md-option>
+      </md-select>
+    </md-field>
 
-    <button>Enviar</button>
+    <div>
+      <label class="genero">Género</label>
+
+      <md-radio v-model="genero" value="Femenino" class="md-primary">Femenino</md-radio>
+      <md-radio v-model="genero" value="Masculino" class="md-primary">Masculino</md-radio>
+      <md-radio v-model="genero" value="Otro" class="md-primary">Otro</md-radio>
+    </div>
+
+    <md-field class="comentarios">
+      <label>Comentarios</label>
+      <md-textarea v-model="comentarios"></md-textarea>
+    </md-field>
+
+    <md-dialog-alert
+      :md-active.sync="modal"
+      md-title="Tus Respuestas:"
+      :md-content="'Nombre: ' + nombre + '<br>Edad: ' + edad + '<br>Ubicación: ' + ubicacion + '<br>Lenguaje Favorito: ' + lenguaje + '<br>Género: ' + genero + '<br>Comentarios: ' + comentarios"
+    />
+    <md-button class="md-raised" @click="modal = true">Enviar</md-button>
   </div>
 </template>
 
@@ -34,19 +60,39 @@ export default {
   name: "Formularios",
   props: {},
   data() {
-      return {
-        nombre: '',
-        ubicacion: '',
-        lenguaje: '',
-        comentarios: ''
-      }     
+    return {
+      nombre: "",
+      ubicacion: "",
+      edad: "",
+      genero: "",
+      lenguaje: "",
+      comentarios: "",
+      modal: false
+    };
   }
 };
 </script>
 
 <style scoped>
-    button {
-        display: block;
-        text-align: center;
-    }
+.md-button.md-theme-default.md-raised:not([disabled]) {
+  background-color: #5f015ab0;
+}
+.md-field.md-has-textarea:not(.md-autogrow) .md-textarea {
+  min-height: 3rem;
+  resize: none;
+}
+.genero {
+  position: relative;
+  display: block;
+  text-align: left;
+  font-size: 16px;
+  line-height: 20px;
+  color: rgba(255, 255, 255, 0.7);
+}
+.md-radio {
+  display: flex;
+}
+.comentarios {
+  top: 10px;
+}
 </style>
