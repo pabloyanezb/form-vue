d5fc1d7ecf3df553967f631ab186bb5d854de8b6
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-07-14 12:17:08 -0400

    Primer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="background">
+    <div class="container">
+      <h2>
+        Encuesta del Dojo
+      </h2>
+      <Formularios />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formularios from "./components/Formularios.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Formularios
+  },
+  data: () => {
+    return {
+      
+    };
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0 !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.background {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(to top, #360033, #0b8793);
+}
+.container {
+  background-color: rgba(53, 53, 53, 0.102);
+  border-radius: 10px;
+  padding: 25px;
+  margin-top: 3rem;
+  backdrop-filter: blur(5px);
+  box-shadow: -7px -7px 23px -4px rgba(0, 0, 0, 0.2);
+  max-width: 280px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  color: #e8f9ff;
+}
+h2 {
+  margin-bottom: 2rem;
+  text-align: center;
 }
 </style>
---
+++ src/components/Formularios.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <p>Tu Nombre:</p>
+    <input type="text" v-model="nombre"/>
+
+    <p>Ubicación:</p>
+    <select v-model="ubicacion">
+      <option value="antofagasta">Antofagasta</option>
+      <option value="valparaiso">Valparaíso</option>
+      <option value="santiago">Santiago</option>
+      <option value="concepcion">Concepción</option>
+      <option value="valdivia">Valdivia</option>
+    </select>
+
+    <p>Lenguaje Favorito:</p>
+    <select v-model="lenguaje">
+      <option value="javascript">Javascript</option>
+      <option value="java">Java</option>
+      <option value="python">Python</option>
+      <option value="c#">C#</option>
+      <option value="swift">Swift</option>
+      <option value="ruby">Ruby</option>
+    </select>
+
+    <p>Comentarios:</p>
+    <textarea v-model="comentarios"></textarea>
+
+    <button>Enviar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Formularios",
+  props: {},
+  data() {
+      return {
+        nombre: '',
+        ubicacion: '',
+        lenguaje: '',
+        comentarios: ''
+      }     
+  }
+};
+</script>
+
+<style scoped>
+    button {
+        display: block;
+        text-align: center;
+    }
+</style>
