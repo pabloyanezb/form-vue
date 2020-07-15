ae90a017d1b01500414eeeffc2bb9c5c482ab03b
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-07-15 11:47:21 -0400

    ADD: Validación de Formulario al presionar botón

--- src/components/Formularios.vue
+++ src/components/Formularios.vue
@@ -1,57 +1,60 @@
 <template>
   <div>
-    <md-field>
-      <label>Nombre</label>
-      <md-input type="text" v-model="nombre" />
-    </md-field>
+    <form action="submit"  @submit="enviar">
+      <md-field>
+        <label>Nombre</label>
+        <md-input type="text" v-model="nombre" required/>
+      </md-field>
 
-    <md-field>
-      <label>Edad</label>
-      <md-input type="number" v-model="edad" />
-    </md-field>
+      <md-field>
+        <label>Edad</label>
+        <md-input type="number" v-model="edad" required min="18" max="100"/>
+      </md-field>
 
-    <md-field>
-      <label>Ubicación</label>
-      <md-select v-model="ubicacion">
-        <md-option value="Antofagasta">Antofagasta</md-option>
-        <md-option value="Valparaíso">Valparaíso</md-option>
-        <md-option value="Santiago">Santiago</md-option>
-        <md-option value="Concepción">Concepción</md-option>
-        <md-option value="Valdivia">Valdivia</md-option>
-      </md-select>
-    </md-field>
+      <md-field>
+        <label>Ubicación</label>
+        <!-- required por alguna razón no funciona en md-select -->
+        <md-select v-model="ubicacion"> 
+          <md-option value="Antofagasta">Antofagasta</md-option>
+          <md-option value="Valparaíso">Valparaíso</md-option>
+          <md-option value="Santiago">Santiago</md-option>
+          <md-option value="Concepción">Concepción</md-option>
+          <md-option value="Valdivia">Valdivia</md-option>
+        </md-select>
+      </md-field>
 
-    <md-field>
-      <label>Lenguaje Favorito</label>
-      <md-select v-model="lenguaje">
-        <md-option value="JavaScript">JavaScript</md-option>
-        <md-option value="Java">Java</md-option>
-        <md-option value="Python">Python</md-option>
-        <md-option value="C#">C#</md-option>
-        <md-option value="Swift">Swift</md-option>
-        <md-option value="Ruby">Ruby</md-option>
-      </md-select>
-    </md-field>
+      <md-field>
+        <label>Lenguaje Favorito</label>
+        <!-- required por alguna razón no funciona en md-select -->
+        <md-select v-model="lenguaje" >
+          <md-option value="JavaScript">JavaScript</md-option>
+          <md-option value="Java">Java</md-option>
+          <md-option value="Python">Python</md-option>
+          <md-option value="C#">C#</md-option>
+          <md-option value="Swift">Swift</md-option>
+          <md-option value="Ruby">Ruby</md-option>
+        </md-select>
+      </md-field>
 
-    <div>
-      <label class="genero">Género</label>
+      <div>
+        <label class="genero">Género</label>
 
-      <md-radio v-model="genero" value="Femenino" class="md-primary">Femenino</md-radio>
-      <md-radio v-model="genero" value="Masculino" class="md-primary">Masculino</md-radio>
-      <md-radio v-model="genero" value="Otro" class="md-primary">Otro</md-radio>
-    </div>
+        <md-radio v-model="genero" value="Femenino" class="md-primary">Femenino</md-radio>
+        <md-radio v-model="genero" value="Masculino" class="md-primary">Masculino</md-radio>
+        <md-radio v-model="genero" value="Otro" class="md-primary">Otro</md-radio>
+      </div>
 
-    <md-field class="comentarios">
-      <label>Comentarios</label>
-      <md-textarea v-model="comentarios"></md-textarea>
-    </md-field>
+      <md-field class="comentarios">
+        <label>Comentarios</label>
+        <md-textarea v-model="comentarios"></md-textarea>
+      </md-field>
 
-    <md-dialog-alert
-      :md-active.sync="modal"
-      md-title="Tus Respuestas:"
-      :md-content="'Nombre: ' + nombre + '<br>Edad: ' + edad + '<br>Ubicación: ' + ubicacion + '<br>Lenguaje Favorito: ' + lenguaje + '<br>Género: ' + genero + '<br>Comentarios: ' + comentarios"
-    />
-    <md-button class="md-raised" @click="modal = true">Enviar</md-button>
+      <md-dialog-alert
+        :md-active.sync="modal"
+        md-title="Tus Respuestas:"
+        :md-content="'Nombre: ' + nombre + '<br>Edad: ' + edad + '<br>Ubicación: ' + ubicacion + '<br>Lenguaje Favorito: ' + lenguaje + '<br>Género: ' + genero + '<br>Comentarios: ' + comentarios"/>
+      <md-button type="submit" class="md-raised" >Enviar</md-button>
+    </form>
   </div>
 </template>
 
@@ -69,6 +72,12 @@ export default {
       comentarios: "",
       modal: false
     };
+  },
+  methods: {
+    enviar: function(ev) {
+      ev.preventDefault();
+      this.modal="true";
+    }
   }
 };
 </script>
